a3a9f3ef38c4f247ff47e503f6d4b56dd167302c
Casimir von Meyer zu Knonow <casimir@meyer-knonow.de>
2022-11-30 14:22:30 +0100

    map home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @parties = Party.all
+    @markers = @parties.geocoded.map do |party|
+      { lat: party.latitude, lng: party.longitude }
+    end
   end
 end
---
+++ db/migrate/20221130112407_add_address_to_parties.rb
@@ -0,0 +1,5 @@
+class AddAddressToParties < ActiveRecord::Migration[7.0]
+  def change
+    add_column :parties, :address, :string
+  end
+end
