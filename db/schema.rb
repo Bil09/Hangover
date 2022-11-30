31549d26649c92769431ee18e8e5b8c71e20719c
Riptagon <maximilian@maeder-karlsruhe.de>
2022-11-30 12:16:04 +0100

    navbar add part 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_162415) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_164608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,13 +21,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_162415) do
     t.index ["party_id"], name: "index_chatrooms_on_party_id"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.boolean "confirmed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.bigint "chatroom_id", null: false
     t.bigint "participent_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-      t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["participent_id"], name: "index_messages_on_participent_id"
   end
 
@@ -66,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_162415) do
   end
 
   add_foreign_key "chatrooms", "parties"
+  add_foreign_key "invitations", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "participents"
   add_foreign_key "participents", "parties"
