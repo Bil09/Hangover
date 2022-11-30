60b258203368944ff65decb739acf2dd2584b2b1
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-11-30 11:56:00 +0100

    party controller new and create

---
+++ app/controllers/parties_controller.rb
@@ -0,0 +1,21 @@
+class PartiesController < ApplicationController
+  def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.new party_params
+    @party.user = current_user
+    if @party.save
+      redirect_to new_partecipants_path
+    else
+      render :new, :unproccesable_entity
+    end
+  end
+
+  private
+
+  def party_params
+    params.require(:party, :latitude, :longitude).permit(:start_time)
+  end
+end
