bc9ce55bfa6357bd75e70011b3160b58642e04f1
Casimir von Meyer zu Knonow <casimir@meyer-knonow.de>
2022-12-01 10:56:47 +0100

    add friend

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,17 @@
 class InvitationsController < ApplicationController
+
   def new
     @invitation = Invitation.new
   end
 
   def create
     friend = User.find_by(email: params[:email])
-    current_user.send_invitation friend
-    redirect_to root_path
+    if friend
+      current_user.send_invitation friend
+      redirect_to root_path
+    else
+      render :new, status: :bad_request
+      flash[:error] = "No User with this email"
+    end
   end
 end
