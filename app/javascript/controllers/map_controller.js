29b64407ec8844bf751d32e59b0a257a30534de5
Riptagon <maximilian@maeder-karlsruhe.de>
2022-12-01 15:35:51 +0100

    New location css

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,25 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    apiKey: String,
-    markers: Array
-  }
+    static values = {
+        apiKey: String,
+        markers: Array
+    }
 
-  connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    connect() {
+        mapboxgl.accessToken = this.apiKeyValue
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/riptagon/clb3favsa000415qvnw2p2sux"
-    })
-    this.#addMarkersToMap()
-  }
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
-    })
-  }
+        this.map = new mapboxgl.Map({
+            container: this.element,
+            style: "mapbox://styles/riptagon/clb3favsa000415qvnw2p2sux"
+        })
+        this.#addMarkersToMap()
+    }
+    addMarkersToMap() {
+        this.markersValue.forEach((marker) => {
+            new mapboxgl.Marker()
+                .setLngLat([marker.lng, marker.lat])
+                .addTo(this.map)
+        })
+    }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "map", to: "maps#index"
+  get "party", to: "parties#new"
 
   resources :invitations, only: [:new, :create]
 
