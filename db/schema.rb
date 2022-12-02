b773378bde718841395b7b5a04a377ffc49e1a55
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-12-02 14:47:29 +0100

    final touch(demo demo day)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_103919) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_130016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_103919) do
   create_table "participants", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "party_id", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["party_id"], name: "index_participants_on_party_id"
