73551bfe8f45b30a8922c84fe366558f432e29af
Riptagon <maximilian@maeder-karlsruhe.de>
2022-12-06 14:55:33 +0100

    added favicon and pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << /\A[a-z\d-]+\.loca\.lt\z/
+  #config.hosts << "https://cute-suits-happen-5-253-115-96.loca.lt"
 end
