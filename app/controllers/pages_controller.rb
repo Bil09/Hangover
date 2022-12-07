f796823dd3925d91223582339b42784ff490bce9
Riptagon <maximilian@maeder-karlsruhe.de>
2022-12-07 10:19:50 +0100

    dropdown fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,21 @@ class PagesController < ApplicationController
       {
         lat: party.latitude,
         lng: party.longitude,
-        info_window: render_to_string(partial: "info_window", locals: {party: party })
+        info_window: render_to_string(partial: "info_window", locals: {party: party }),
+        # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
       }
     end
+
+    # locations = Location.all
+    # location_markers = locations.geocoded.map do |party|
+    #   {
+    #     lat: party.latitude,
+    #     lng: party.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: {party: party }),
+    #     image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
+    #   }
+    # end
+    # @markers = @markers + location_markers
   end
 
   def welcome
