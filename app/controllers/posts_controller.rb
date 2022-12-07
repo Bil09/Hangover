1570f3f1cda2b52af54d98d259acd839ca91a682
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-12-07 15:09:06 +0100

    feed logic and frontend

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.new
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new post_params
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(photos: [])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_132958) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_084400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_132958) do
     t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer "like"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -114,4 +122,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_132958) do
   add_foreign_key "participants", "parties"
   add_foreign_key "participants", "users"
   add_foreign_key "parties", "users"
+  add_foreign_key "posts", "users"
 end
